084d4de18d787a00cc48280ed5b9010991cfcb92
Siripa-Maneein <siripa.m@ku.th>
2023-02-08 13:04:46 +0700

    add docstrings

--- routing/routing_room.py
+++ routing/routing_room.py
@@ -7,7 +7,7 @@ collection = database.client["exceed06"]["smart_home"]
 
 @router.post("/manual/turn_on/{id}")
 def turn_on_manually(id: int):
-    # Change manual in the database of that room to On.
+    """Set status of bulb with given id to True."""
     collection.update_one(
         {"id": id, "mode": "manual", "status": False},
         {"$set": {"status": True}},
@@ -18,7 +18,7 @@ def turn_on_manually(id: int):
 
 @router.post("/manual/turn_off/{id}")
 def turn_off_manually(id: int):
-    # Change manual in the database of that room to Off.
+    """Set status of bulb with given id to False."""
     collection.update_one(
         {"id": id, "mode": "manual", "status": True},
         {"$set": {"status": False}},
@@ -29,20 +29,21 @@ def turn_off_manually(id: int):
 
 @router.post("/change_mode/{id}/{mode}/")
 def change_mode(id: int, mode: str):
-    # disables Manual button and change Auto in the database of that room to On.
+    """Set Mode of bulb with that id to the given mode ('auto'/ 'manual')."""
     collection.update_one({"id": id}, {"$set": {"mode": mode}}, upsert=False)
     return collection.find_one({"id": id}, {"_id": 0})
 
 
 @router.post("/change_brightness/{id}/{brightness}/")
 def change_brightness(id: int, brightness: int):
+    """Set brightness of bulb with that id to the given brightness."""
     collection.update_one({"id": id}, {"$set": {"brightness": brightness}})
     return collection.find_one({"id": id}, {"_id": 0})
 
 
 @router.get("/get_all_bulbs_info/")
 def get_all_bulbs_info():
-    # get all bulbs info
+    """Return information about all light bulbs"""
     result = {}
     for i in collection.find({}, {"_id": False}):
         result["room_" + str(i["id"])] = i
