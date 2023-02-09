9b4dfce32a8874e0b6471943e7d3160967c10fcc
ItsZodiaX <59441461+ItsZodiaX@users.noreply.github.com>
2023-02-09 09:17:42 +0700

    database.py: Update database client

--- config/database.py
+++ config/database.py
@@ -4,9 +4,12 @@ import os
 import urllib
 
 load_dotenv(".env")
+_host = urllib.parse.quote(os.getenv("host"))
+_port = urllib.parse.quote(os.getenv("port"))
 _username = urllib.parse.quote(os.getenv("user"))
 _password = urllib.parse.quote(os.getenv("password"))
+_database = urllib.parse.quote(os.getenv("database"))
 
-client = MongoClient(
-    "mongo.exceed19.online", port=8443, username=_username, password=_password
-)
+client = MongoClient("mongodb://" + _username + ":" + _password + "@" + _host + ":" + _port + "/" + _database)
+
+# client = MongoClient("localhost", port=27017)
